d91120f842c07d15496ad323eb9fbd79d3cdaf90
Milen Tecle <m_tecle@hotmail.com>
2024-01-10 19:20:05 +0000

    Add the category and item models to the inventory app

--- inventory/models.py
+++ inventory/models.py
@@ -1,12 +1,30 @@
 from django.db import models
-from django.contrib.auth.models import get_user_model
+from django.contrib.auth import get_user_model
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
+
+
 class Inventory(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    category = models.ForeignKey(Category, on_delete=models)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+
+
+class Items(models.Model):
+    name = models.CharField(max_length=255)
+    inventory = models.ForeignKey(Inventory, on_delete=models.CASCADE, related_name='items')
+
+    def __str__(self):
+        return self.name
+
+
+
+
