ec3b72432ba02c9f05bb1e69494d80bce278983a
Milen Tecle <m_tecle@hotmail.com>
2024-01-10 14:40:38 +0000

    Create the inventory model

--- inventory/models.py
+++ inventory/models.py
@@ -1,5 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import get_user_model
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Inventory(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models)
+
+    def __str__(self):
+        return self.name
