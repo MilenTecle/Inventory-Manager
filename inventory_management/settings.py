3458be2d8e29d686551b00c797f64781d4534cdc
Milen Tecle <m_tecle@hotmail.com>
2024-01-11 14:05:06 +0000

    Adjust the inventory html and add javascript code to toggle form and add,delete and edit items

--- inventory_management/settings.py
+++ inventory_management/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-milentecle-inventoryman-b67k1i7mdri.ws-eu107.gitpod.io', '.herokuapp.com',]
 
--- static/js/inventory.js
+++ static/js/inventory.js
@@ -1,8 +1,40 @@
-/* Handles the click event to render the inventory form */
-
 document.addEventListener('DOMContentLoaded', function () {
-    const form = document.getElementById('inventory-form');
+const form = document.getElementById('inventory-form');
+const itemsContainer = document.getElementById('items');
+const addItem = document.getElementById('add-item')
+
+
+
+/* Handles the click event to render the inventory form */
     document.getElementById('toggle-form').addEventListener('click', function() {
-        form.classList.toggle('hide');
+        form.classList.toggle('hide');;
+        addItem.classList.toggle('hide')
+    });
+
+
+    /* Adding items when clicking on icon */
+    addItem.addEventListener('click', function() {
+        const newItem = document.createElement('div');
+        newItem.classList.add('item');
+        newItem.innerHTML = `
+                <input type="text" name="items" class="form-control item-name" placeholder="Item name">
+                <i class="fa-solid fa-pencil edit-icon"></i>
+                <i class="fa-regular fa-trash-can delete-icon"></i>`;
+        itemsContainer.appendChild(newItem);
+
+
+        /* Edit icon event listener */
+        const editIcon = newItem.querySelector('.edit-icon');
+        editIcon.addEventListener('click', function() {
+            const inputField = newItem.querySelector('.item-name');
+            inputField.removeAttribute('readonly');
+            inputField.focus();
+        });
+
+        /* Delete icon event listener */
+        const deleteIcon = newItem.querySelector('.delete-icon');
+        deleteIcon.addEventListener('click', function() {
+            newItem.remove();
+        });
     });
 });
