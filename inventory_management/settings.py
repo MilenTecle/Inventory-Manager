d6f5d1c9c8e49535c34992b940e72ca0b335f0f0
Milen Tecle <m_tecle@hotmail.com>
2024-01-16 08:27:14 +0000

    Change debug to false for deployment

--- inventory_management/settings.py
+++ inventory_management/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-milentecle-inventoryman-zn1e309kv3x.ws-eu107.gitpod.io', '.herokuapp.com',]
 
