be84834a05cef9cdf4648afa30e2e1c4debf379c
Milen Tecle <m_tecle@hotmail.com>
2024-01-22 10:26:56 +0000

    Update the staticfiles for heroku

--- staticfiles/js/inventory.js
+++ staticfiles/js/inventory.js
@@ -1,22 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
 const form = document.getElementById('inventory-form');
-const itemsContainer = document.getElementById('items');
+/*const itemsContainer = document.getElementById('items');
 const addItem = document.getElementById('add-item')
-const saveBtn = document.getElementById('save')
+const saveBtn = document.getElementById('save')*/
 const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
 let deleteItem = null; // variable to keep track of which item to delete
+let deleteInventory = null
 
 
 /* Handles the click event to render the inventory form */
-    document.getElementById('toggle-form').addEventListener('click', function() {
+    /*document.getElementById('toggle-form').addEventListener('click', function() {
         form.classList.toggle('hide')
-        addItem.classList.toggle('hide')
+        /* addItem.classList.toggle('hide')
         itemsContainer.classList.toggle('hide')
         saveBtn.classList.toggle('hide')
-    });
+    })*/
 
 
-    /* Adding items when clicking on icon */
+    /* Adding items when clicking on icon
     addItem.addEventListener('click', function() {
         const newItem = document.createElement('div');
         newItem.classList.add('item');
@@ -24,30 +25,51 @@ let deleteItem = null; // variable to keep track of which item to delete
                 <input type="text" name="items" class="form-control item-name" placeholder="Item name">
                 <i class="fa-solid fa-pencil edit-icon"></i>
                 <i class="fa-regular fa-trash-can delete-icon"></i>`;
-        itemsContainer.appendChild(newItem);
+        itemsContainer.appendChild(newItem); */
 
 
         /* Edit icon event listener */
-        const editIcon = newItem.querySelector('.edit-icon');
-        editIcon.addEventListener('click', function() {
-            const inputField = newItem.querySelector('.item-name');
+        const editIcons = document.querySelectorAll('.edit-icon');
+        editIcons.forEach(editIcon => {
+            editIcon.addEventListener('click', function(event) {
+            event.preventDefault();
+            const formRow = this.closest('.form-row');
+            const inputField = formRow.querySelector('.item-name');
             inputField.removeAttribute('readonly');
             inputField.focus();
         });
+    });
+
 
         /* Delete icon event listener */
-        const deleteIcon = newItem.querySelector('.delete-icon');
+        const deleteIcons = document.querySelectorAll('.delete-icon');
+        deleteIcons.forEach(deleteIcon => {
         deleteIcon.addEventListener('click', function() {
-            deleteItem = newItem;
+            deleteItem = this.closest('.form-row');
             deleteModal.show();
         });
     });
+
+        /* Delete entire list event listener */
+        const deleteList = document.querySelector('.delete-list');
+        if (deleteList) {
+            deleteList.addEventListener('click', function() {
+                deleteInventory = this.closest('.saved-list');
+                deleteModal.show()
+        });
+    }
 
     document.getElementById('deleteConfirm').addEventListener('click', function() {
         if (deleteItem) {
             deleteItem.remove();
             deleteItem = null; // reset the variable
-            deleteModal.hide()
+
         }
-    });
+        if (deleteInventory) {
+            deleteInventory.remove();
+            deleteInventory = null;
+        }
+
+        deleteModal.hide()
+     });
 });
